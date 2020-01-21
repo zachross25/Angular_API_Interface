02cfacbebe494134e71bd16c8c356fd6b9fea2a4
megido_ <megido666@gmail.com>
2020-01-21 08:26:02 +0200

    fix app path in express

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo'));
+app.use(express.static(__dirname + '/dist/AngularTestAPI'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/demo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/AngularTestAPI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
