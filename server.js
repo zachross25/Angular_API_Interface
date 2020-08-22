ba464fe5055e3921bf660d5894e963af1a320f0f
ross <rosstindev@gmail.com>
2020-08-22 23:16:59 +0300

    port changed

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 8080);
 console.log('Console Listening');
