2bff320a93ae93f18685b1db508304da6fd2deea
rostyslavnikitin <rosstindev@gmail.com>
2020-11-11 11:53:51 +0200

    update comments component

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,29 +1,24 @@
-import { Component, Input, OnInit, Self } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommentsService } from '../services/comments.service';
 import { ActivatedRoute } from '@angular/router';
 import { Comment } from '../objects/comment';
-import { NgOnDestroy } from '../ngondestroy';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css'],
-  providers: [ NgOnDestroy ]
+  styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
   @Input() postId: number;
   comments: Comment[] = [];
 
-  constructor(@Self()
-              private ngOnDestroy$: NgOnDestroy,
+  constructor(
               private route: ActivatedRoute,
               private commS: CommentsService
   ) { }
 
   ngOnInit() {
       this.commS.GetComments(String(this.postId))
-        .pipe(takeUntil(this.ngOnDestroy$))
-        .subscribe(comments => this.comments =  comments);
+        .subscribe(comments => this.comments = comments);
   }
 }
